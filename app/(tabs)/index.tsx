fea5b5f592f580838f047cb0ee5e09e20dbbd10d
Brian Donnoe <dunow33@hotmail.com>
2024-06-05 14:34:47 -0400

    more practice with various components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,78 @@
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, StatusBar, SafeAreaView, TextInput, Switch } from 'react-native';
 import { useState } from 'react';
 //import Greet from '../../components/Greet';
-import pokemonList from '../../data.json'
-
+import pokemonList from '../../data.json';
+import groupedPokemonList from '../../grouped-data.json'
 
 export default function HomeScreen() {
+
+  const [name, setName] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-      <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <TextInput 
+        style={styles.input} 
+        value={name} 
+        onChangeText={setName}
+        placeholder="brian@hotmail.com"
+        // autoCorrect={false}
+        // autoCapitalize="none"
+        // secureTextEntry
+        // keyboardType="numeric" 
+        />
+      <TextInput 
+        style={[styles.input, styles.multilineText]} 
+        placeholder="message" 
+        multiline 
+      />
+      <Text style={styles.text}>My name is {name}</Text>
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark Mode</Text>
+        <Switch 
+          value={isDarkMode} 
+          onValueChange={() => setIsDarkMode((previousState) => !previousState)}
+          trackColor={{ false: "#767577", true: "lightblue"}}
+          thumbColor="#f4f3f4"
+           />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'eggshell',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1
+  },
+  text: {
+    fontSize: 30,
+    padding: 10
+  },
+  multilineText: {
+    minHeight: 100,
+    textAlignVertical: "top"
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10
+  },
+});
+
+// export default function HomeScreen() {
+//   return (
+//       <View style={styles.container}>
         {/*<ScrollView style={styles.scrollView}>
         {
           pokemonList.map(pokemon => {
@@ -18,8 +84,8 @@ export default function HomeScreen() {
             )
           })}
         </ScrollView>*/}
-        <View style={styles.scrollView}>
-          <FlatList 
+       // <View style={styles.scrollView}>
+          {/*<FlatList 
             data={pokemonList}
             renderItem={({item}) => {
               return (
@@ -34,11 +100,29 @@ export default function HomeScreen() {
             ListEmptyComponent={<Text>No items found</Text>}
             ListHeaderComponent={<Text style={styles.headerText}>Pokemon List</Text>}
             ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
-          />
-        </View>
-      </View>
-  )
-}
+          />*/}
+
+//           <SectionList
+//             sections={groupedPokemonList}
+//             renderItem={({ item }) => {
+//               return (
+//                 <View style={styles.card}>
+//                   <Text style={styles.cardText}>{item}</Text>
+//                 </View>
+//               );
+//             }}
+//             renderSectionHeader={({ section }) => (
+//               <Text style={styles.sectionHeaderText}>{section.type}</Text>
+//             )}
+
+//             ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+//             SectionSeparatorComponent={() => <View style={{ height: 16 }} />}
+
+//           />
+//         </View>
+//       </View>
+//   )
+// }
 
 
 //export default function HomeScreen() {
@@ -102,35 +186,3 @@ export default function HomeScreen() {
 //     </View>
 //   );
 // }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  scrollView: {
-    paddingHorizontal: 16
-  },
-  card: {
-    backgroundColor: "white",
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    //marginBottom: 16
-  },
-  cardText: {
-    fontSize: 30
-  },
-  headerText: {
-    fontSize: 24,
-    textAlign: "center",
-    marginBottom: 12
-  },
-  footerText: {
-    fontSize: 24,
-    textAlign: "center",
-    marginBottom: 12
-  },
-});
