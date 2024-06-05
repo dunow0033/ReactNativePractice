0b26eacf4103ce3dd477107c194d0b00f56a474d
Brian Donnoe <dunow33@hotmail.com>
2024-06-04 22:50:47 -0400

    just practicing here

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,136 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { useState } from 'react';
+//import Greet from '../../components/Greet';
+import pokemonList from '../../data.json'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+      <View style={styles.container}>
+        {/*<ScrollView style={styles.scrollView}>
+        {
+          pokemonList.map(pokemon => {
+            return (
+              <View style={styles.card} key={pokemon.id}>
+                <Text style={styles.cardText}>{pokemon.type}</Text>
+                <Text style={styles.cardText}>{pokemon.name}</Text>
+              </View>
+            )
+          })}
+        </ScrollView>*/}
+        <View style={styles.scrollView}>
+          <FlatList 
+            data={pokemonList}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.card} key={item.id}>
+                  <Text style={styles.cardText}>{item.type}</Text>
+                  <Text style={styles.cardText}>{item.name}</Text>
+                </View>
+              );
+            }} 
+            keyExtractor={(item) => item.id.toString()}
+            ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+            ListEmptyComponent={<Text>No items found</Text>}
+            ListHeaderComponent={<Text style={styles.headerText}>Pokemon List</Text>}
+            ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
+          />
+        </View>
+      </View>
+  )
 }
 
+
+//export default function HomeScreen() {
+
+  // return (
+  //   <View style={{ flex: 1, backgroundColor: "eggshell", padding: 60 }}>
+  //     <Greet name="Bruce Wayne" />
+  //     <Greet name="Clark Kent" />
+  //   </View>
+  // )
+//}
+
+// const showAlert = (message: string, description = '') => {
+//   alert(description ? `${message}\n${description}` : message);
+// }
+
+// return (
+//     <View style={{ flex: 1, backgroundColor: "eggshell", padding: 60 }}>
+//       <Button title="Alert" onPress={() => alert("Invalid data!!")} />
+//       <Button title="Alert2" onPress={() => showAlert("Invalid data!!", "DOB incorrect")} />
+//     </View>
+//   )
+
+// export default function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, backgroundColor: "eggshell", padding: 60 }}>
+//       <ActivityIndicator />
+//       <ActivityIndicator size="large" />
+//       <ActivityIndicator size="large" color="midnightblue" />
+//       <ActivityIndicator size="large" color="midnightblue" animating={false} />
+//     </View>
+//   );
+// }
+
+// export default function HomeScreen() {
+
+//   const [isModalVisible, setIsModalVisible] = useState(false);
+
+//   return (
+//     <View style={{ flex: 1, backgroundColor: "eggshell", padding: 60 }}>
+//       <Button
+//         title="Press"
+//         onPress={() => setIsModalVisible(true)}
+//         color="black"
+//         />
+//         <Modal 
+//           visible={isModalVisible} 
+//           onRequestClose={() => setIsModalVisible(false)}
+//           animationType="slide"
+//           presentationStyle='pageSheet'
+//         >
+//           <View style={{ flex: 1, backgroundColor: "eggshell", padding: 60 }}>
+//             <Text>Modal content</Text>
+//             <Button 
+//               title="Close" 
+//               color="midnightblue" 
+//               onPress={() => setIsModalVisible(false)} 
+//               />
+//           </View>
+//         </Modal>
+//     </View>
+//   );
+// }
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  scrollView: {
+    paddingHorizontal: 16
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    //marginBottom: 16
+  },
+  cardText: {
+    fontSize: 30
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12
   },
 });
